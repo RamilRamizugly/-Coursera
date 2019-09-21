90e2e2b2e247439ea8569d2d5da348971ef88be6
RamilRamizugly <brt2003grr@gmail.com>
2019-09-21 09:45:53 +0000

    calk_age()

--- req/friends.py
+++ req/friends.py
@@ -1,11 +1,19 @@
-import requests as req
+import requests
 
 token ='5e7529c75e7529c75e7529c7bf5e19d46c55e755e7529c703f0cf33dd30bc75e4f35500'
+uid = 1234
+def calc_age():
+    r = requests.get('https://api.vk.com/method/users.get?user_ids=1234&access_token=17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711&v=5.101')
+    print(r.text)
 
-def calc_age(uid):
-    pass
+
+calc_age()
 
 
-if __name__ == '__main__':
-    res = calc_age('reigning')
-    print(res)
+
+'''def main():
+
+    r = requests.get('https://api.vk.com/method/users.get?user_ids=1')
+    print(r)
+
+main()'''
