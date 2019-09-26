5737cf4bbc6eb5de131397eab0c7bdcd0c7279b8
GanievRamil <brt2003grr@gmail.ru>
2019-09-27 00:16:27 +0300

    dikt items

--- req/friends.py
+++ req/friends.py
@@ -1,22 +1,27 @@
 import requests
+import pydash
 
+
+access_token = '5e7529c75e7529c75e7529c7bf5e19d46c55e755e7529c703f0cf33dd30bc75e4f35500'
+api_version = '5.101'
 token ='5e7529c75e7529c75e7529c7bf5e19d46c55e755e7529c703f0cf33dd30bc75e4f35500'
 uid = 1234
 def calc_age():
-    #r = requests.get('https://api.vk.com/method/users.get?user_ids=13586336&access_token=17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711&v=5.101')
-    r = requests.get(
-        'https://api.vk.com/method/friends.get?user_id=13586336&fields=bdate&access_token=17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711&v=5.101')
+    r = requests.get('https://api.vk.com/method/friends.get?user_id=13586336&fields=bdate&access_token=17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711&v=5.101')
+    search_dict = r.json()
+    response_key = search_dict.get('response')
+    items_key = response_key.get('items')
 
-    print(r.text)
+    list_dikt= items_key[3]
+    print(list_dikt)
+    print(type(list_dikt))
 
 
 calc_age()
 
 
 
-'''def main():
 
-    r = requests.get('https://api.vk.com/method/users.get?user_ids=1')
-    print(r)
 
-main()'''
+
+
