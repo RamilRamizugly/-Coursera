06060eb71789d754763466accfcd91b150b580bd
GanievRamil <brt2003grr@gmail.ru>
2019-10-02 01:01:34 +0300

    date times format

--- req/friends.py
+++ req/friends.py
@@ -1,6 +1,9 @@
 import requests
-import pydash
-
+from datetime import datetime
+import locale
+locale.setlocale(locale.LC_ALL, "")
+now = datetime.now()
+today = datetime.today()
 
 access_token = '5e7529c75e7529c75e7529c7bf5e19d46c55e755e7529c703f0cf33dd30bc75e4f35500'
 api_version = '5.101'
@@ -9,19 +12,18 @@ uid = 1234
 def calc_age():
     r = requests.get('https://api.vk.com/method/friends.get?user_id=13586336&fields=bdate&access_token=17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711&v=5.101')
     search_dict = r.json()
-    response_key = search_dict.get('response')
-    items_key = response_key.get('items')
+    for item in search_dict['response']['items']:
+        bdates = item.get('bdate')
 
+        >> > import dateutil
+        >> > dates = []
+        >> > dates.append('12 1 2017')
+        >> > dates.append('1 1 2017')
+        >> > dates.append('1 12 2017')
+        >> > dates.append('June 1 2017 1:30:00AM')
+        >> > [parser.parse(x) for x in dates]
 
-    #list_dikt= items_key[1]
-    iter_item = iter(items_key)
-    print(next(iter_item,None))
-
-
-    #print(list_dikt)
-    #print(type(list_dikt))
-
-
+        t = DateTime.ParseExact("24/01/2013", "dd/MM/yyyy", CultureInfo.InvariantCulture);
 calc_age()
 
 
